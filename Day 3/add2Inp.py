5ca4b2810a1a88732a9b6795b363650509d1f253
Aashritha <aashrithabadampudi@gmail.com>
2025-12-07 20:36:56 +0530

    Added

---
+++ Day 3/add2Inp.py
@@ -0,0 +1,4 @@
+#Add two numbers using input
+num1=int(input("Enter num1:\n"))
+num2=int(input("Enter num2:\n"))
+print("Sum of num1 and num2 is:\n",num1+num2)
---
+++ Day 3/input.py
@@ -0,0 +1,10 @@
+Name=input("Enter your name\n")
+print ("Your name is:",Name)
+
+Age=int(input("Enter your age\n"))
+print("Your age is:",Age)
+
+Height=float(input("Enter your height\n"))
+print("Your height is:",Height)
+
+
