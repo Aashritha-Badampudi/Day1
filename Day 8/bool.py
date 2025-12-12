5dc7747ea3aa6213355cfcb10a66df5db6031ec3
Aashritha <aashrithabadampudi@gmail.com>
2025-12-12 19:34:00 +0530

    Added

---
+++ Day 8/bool.py
@@ -0,0 +1,14 @@
+# True or False
+# On or Off
+# 1 or 0
+# Treats these as False and remaining all are True
+# 1️ False
+# 2️ 0 (zero integer)
+# 3️ 0.0 (zero float)
+# 4️ 0j (zero complex number)
+# 5️ "" (empty string)
+# 6️ [] (empty list)
+# 7️ {} (empty dictionary)
+# 8️ set() (empty set)
+# 9️ () (empty tuple)
+# 10 None 
