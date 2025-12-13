57d81afd186aaa06724e68e0d841438bc5a231c2
Aashritha <aashrithabadampudi@gmail.com>
2025-12-13 20:49:43 +0530

    Added

---
+++ Day 8/9bool.py
@@ -0,0 +1,9 @@
+#XOR
+print(True^True^True)      #3 (odd)  True
+print(True^True^False)     #2 (even) False  
+print(True^False^True)     #2 (even) False  
+print(True^False^False)    #1 (odd)	 True  
+print(False^True^True)     #2 (even) False   
+print(False^True^False)    #1 (odd)	 True  
+print(False^False^True)    #1 (odd)  True
+print(False^False^False)   #0 (even) False
