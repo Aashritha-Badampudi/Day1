5c93526656aab5fbd0f4288e2c7f987ae4aa2daa
Aashritha <aashrithabadampudi@gmail.com>
2025-12-13 19:48:19 +0530

    Added

---
+++ Day 8/7bool.py
@@ -0,0 +1,9 @@
+# 3 boolean values using or
+print(True|True|True) 
+print(True|True|False)
+print(True|False|True)
+print(True|False|False)
+print(False|True|True)
+print(False|True|False)
+print(False|False|True)
+print(False|False|False) #Only this becomes false
