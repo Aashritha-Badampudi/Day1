c8fc6c5f3d3b2a334103b207131bcb856d9cd1a7
Aashritha <aashrithabadampudi@gmail.com>
2025-12-14 19:50:42 +0530

    Added

---
+++ Day 9/0lists.py
@@ -0,0 +1,15 @@
+#A list is a sequence data type used to store multiple values in one variable.
+
+# Key Characteristics of Lists
+
+# 1️⃣ Ordered
+# Items keep the same order as you add them
+
+# 2️⃣ Mutable (Changeable)
+# You can modify items after creation
+
+# 3️⃣ Allows duplicates
+# Same value can appear more than once
+
+# 4️⃣ Can store mixed data types
+# int, float, string, boolean, even other lists
