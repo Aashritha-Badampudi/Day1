6746d0404b428dd8319b46158fa6fb8cddb5175a
Aashritha <aashrithabadampudi@gmail.com>
2025-12-16 19:28:46 +0530

    Added

---
+++ Day 11/0 tuple.py
@@ -0,0 +1,11 @@
+"""
+A tuple is a sequence data type used to store multiple values in order.
+Uses ()
+
+Tuples are used when:
+> Data should not change
+> Data must be protected
+> Faster performance is needed
+> You want fixed data
+
+"""
