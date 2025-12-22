c9c6caeb6b4c98b1e84206af5f4dc19226774917
Aashritha <aashrithabadampudi@gmail.com>
2025-12-22 19:46:54 +0530

    Added

---
+++ Day 16/0 MathFun.py
@@ -0,0 +1,25 @@
+"""
+Python provides math functions in two ways:
+1. Built-in math functions (no import needed)
+2. "math" module functions (need import)
+
+********Index*********
+_______________________________________
+| Function           | Purpose        |
+| ------------------ | -------------- |
+| 1. Built-in functions               |
+| `abs()`            | Absolute value |
+| `pow()`            | Power          |
+| `round()`          | Rounding       |
+| `max()`            | Largest        |
+| `min()`            | Smallest       |
+| `sum()`            | Total          |
+| 2. Using math module                |
+| `math.sqrt()`      | Square root    |
+| `math.ceil()`      | Round up       |
+| `math.floor()`     | Round down     |
+| `math.factorial()` | Factorial      |
+| `math.gcd()`       | GCD            |
+| `math.log()`       | Logarithm      |
+|____________________|________________|
+"""
