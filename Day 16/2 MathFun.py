81c4f717abb8edb7507f6da736370b1563fd2705
Aashritha <aashrithabadampudi@gmail.com>
2025-12-22 20:03:19 +0530

    Added

---
+++ Day 16/2 MathFun.py
@@ -0,0 +1,18 @@
+#Using math module
+import math
+print(math.sqrt(144))
+print(math.ceil(4.2))
+print(math.floor(4.2))
+print(math.factorial(6))
+print(math.gcd(12,146))
+print(math.log(20)) #Log( with base e (normal log))
+print(math.log10(20)) #Log( with base anything )
+
+#Trigonometric functions
+print(math.sin(math.radians(90)))
+print(math.cos(math.radians(90)))
+print(math.tan(math.radians(90)))
+
+#Math constants
+print(math.pi)
+print(math.e)
