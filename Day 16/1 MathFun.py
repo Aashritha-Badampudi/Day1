89fbca78833f42b61396813709a942ad58854a04
Aashritha <aashrithabadampudi@gmail.com>
2025-12-22 19:54:52 +0530

    Added

---
+++ Day 16/1 MathFun.py
@@ -0,0 +1,9 @@
+#Built-in functions
+print(abs(-10))
+print(pow(3,2)) # pow(a,b) -> a is base and b is power
+print(round(11.6))
+print(round(11.1))
+print(round(3.111111111119,2)) #by giving ,2 we get only 2 digits after decimal point
+print(max(31,122))
+print(min(31,122))
+print(sum([1,2,3,4]))
