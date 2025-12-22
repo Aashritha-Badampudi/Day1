d5f37d4056929037e1e251b5c6ce1e2fe21c79b6
Aashritha <aashrithabadampudi@gmail.com>
2025-12-22 19:35:14 +0530

    Added

--- Day 15/0 dict.py
+++ Day 15/0 dict.py
@@ -1,2 +1,31 @@
-# 1. What is a Dictionary?
-# A dictionary stores data in key : value pairs.
+"""
+1. What is a Dictionary?
+A dictionary stores data in key : value pairs.
+
+2. Why Use Dictionaries?
+We use dictionaries when:
+Data has meaning
+Each value is identified by a key
+Data is structured
+
+3. Dictionary Characteristics (Important)
+✔ Stores data as key : value
+✔ Keys must be unique
+✔ Values can be duplicated
+✔ Mutable (can change)
+✔ Written using {}
+
+*******Index for Dictionary*********
+1. Creating normal and empty dictionaries
+2. Dictionaries with different data types
+3. Accessing elements in dictionaries using key() and get()
+4. Adding new elements in the dictionary
+5. Updating values in the dictionary
+6. Removing elements using pop(), del[] and clear()
+7. Length of the dictionary
+8. Checking membership
+9. Dictionary methods : keys(), values(), items(), copy()
+10. Nested dictionary
+11. Create dictionary using dict
+12. Convert list to dictionary
+"""
