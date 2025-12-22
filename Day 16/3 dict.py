0d05c99d8b801a997f047685bdfed584b98ec064
Aashritha <aashrithabadampudi@gmail.com>
2025-12-22 18:37:38 +0530

    Added

--- Day 16/3 dict.py
+++ Day 16/3 dict.py
@@ -1,6 +1,11 @@
 #Accessing elements from dictionaries
 
-#Using key name
 student = {"Name": "Aashritha", "Age" : 19}
+
+#Using key name. If key doesn't exist we get error
 print(student["Name"])
 print(student["Age"])
+
+#Using get()
+print(student.get("Name"))
+print(student.get("Marks")) #We get None as o/p no error
