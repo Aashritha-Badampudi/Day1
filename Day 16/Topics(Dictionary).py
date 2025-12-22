bc75508d2809438491b2fb5ccdba34985cebb027
Aashritha <aashrithabadampudi@gmail.com>
2025-12-22 19:30:14 +0530

    Added

---
+++ Day 16/Topics(Dictionary).py
@@ -0,0 +1,14 @@
+"""
+1. Creating normal and empty dictionaries
+2. Dictionaries with different data types
+3. Accessing elements in dictionaries using key() and get()
+4. Adding new elements in the dictionary
+5. Updating values in the dictionary
+6. Removing elements using pop(), del[] and clear()
+7. Length of the dictionary
+8. Checking membership
+9. Dictionary methods : keys(), values(), items(), copy()
+10. Nested dictionary
+11. Create dictionary using dict
+12. Convert list to dictionary
+"""
