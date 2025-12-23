12f9ba171438caaaea57e790332abe8aeb2c1f1c
Aashritha <aashrithabadampudi@gmail.com>
2025-12-23 19:36:42 +0530

    Added

---
+++ Day 17/0 ifelse.py
@@ -0,0 +1,29 @@
+"""
+1. What is if-else?
+   if-else is used to make decisions in a program.
+It tells Python:
+ If this condition is true, do this
+ Otherwise, do something else
+ Python checks conditions, and conditions always give: True or False
+
+ 2. Why do we need if-else?
+    Without if-else, a program:
+    always runs the same way
+    cannot take decisions
+With if-else, a program can:
+    choose different outputs
+    behave like real life logic
+
+    **********Index***********
+    0. Intendation
+    1. if
+    2. if-else
+    3. if-elif-else
+    4. Nested if-else
+    5. Short hand if-else
+    6. Pass statement
+    7. Conditions
+    8. Boolean values
+    9. &|! in if-else
+   10. Challenges
+   """
