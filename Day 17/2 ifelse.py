95ef061b5dea6ed1d2564c16323879330a4414f6
Aashritha <aashrithabadampudi@gmail.com>
2025-12-23 19:43:44 +0530

    Added

---
+++ Day 17/2 ifelse.py
@@ -0,0 +1,12 @@
+"""
+Syntax for if-else:
+if condition:
+    statement
+else:
+    statement
+"""
+age=int(input("Enter your age: \n"))
+if age>=18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
