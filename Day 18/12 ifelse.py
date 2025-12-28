8b5a78d1eb387e37b678f72d73233e53c9abd572
Aashritha <aashrithabadampudi@gmail.com>
2025-12-28 20:20:14 +0530

    Added

---
+++ Day 18/12 ifelse.py
@@ -0,0 +1,7 @@
+temperature=int(input("Enter the temperature: \n"))
+if temperature > 35:
+    print("Weather is hot")
+elif 25<=temperature<=35:
+    print("Weather is normal")
+else:
+    print("Weather is cool")
