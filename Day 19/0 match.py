1c327981e1e2be3aaf5aa88c771312c3b11c1104
Aashritha <aashrithabadampudi@gmail.com>
2025-12-29 19:09:02 +0530

    Added

---
+++ Day 19/0 match.py
@@ -0,0 +1,25 @@
+"""
+1. What is match-case?
+
+match checks a value
+case compares that value with different patterns
+
+👉 When a match is found, that block runs.
+
+>> Syntax:
+
+match variable:
+    case value1:
+        statement
+    case value2:
+        statement
+    case _:
+        statement
+
+>> Explanation
+match variable: → value to check
+
+case value: → possible option
+
+case _: → default (like else)
+"""
