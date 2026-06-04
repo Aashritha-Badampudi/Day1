53a9bd7e5d62c3df0a385f635321c239c82dfb7e
Aashritha <aashrithabadampudi@gmail.com>
2026-06-04 11:27:50 +0530

    Added

--- Day 98/mymodule.py
+++ Day 98/mymodule.py
@@ -1,2 +1,8 @@
 def greeting(name):
     print("Hello, "+ name)
+
+person1={
+    "name":"Honey",
+    "age":20,
+    "country":"India"
+    }
---
+++ Day 98/umodule.py
@@ -0,0 +1,6 @@
+import mymodule
+
+mymodule.greeting("Aashritha")
+
+a=mymodule.person1["age"]
+print(a)
